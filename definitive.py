a9586e12b4e489676f23853f64ac44bd44526159
JeanP09 <juanpaguzman14@gmail.com>
2024-07-30 22:22:35 -0500

    Commit Arreglo 1.1

--- definitive.py
+++ definitive.py
@@ -1,11 +1,14 @@
 import os
 import re
-import pandas as pd
-from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.cell.cell import MergedCell
 from PyPDF2 import PdfReader
 from datetime import datetime
+import win32com.client as win32
 
 directorio = r'C:\Users\user\Documents\Digitalizados_2021\11001333501020180005000\01PrimeraInstancia\C01Principal1'
+ruta_excel_existente = r'C:\Users\user\Documents\Digitalizados_2021\11001333501020180005000\01PrimeraInstancia\FormatoIndiceElectronicoTest.xlsm'
+ruta_excel_nueva = r'C:\Users\user\Documents\Digitalizados_2021\11001333501020180005000\01PrimeraInstancia\C01Principal1\00IndiceElectronico.xlsm'
 
 archivos_info = []
 
@@ -15,33 +18,95 @@ def limpiar_nombre_archivo(nombre_archivo):
     nombre_limpio = nombre_limpio.replace('_', ' ').strip()
     return nombre_limpio
 
+def obtener_tipo_formato(archivo):
+    extension = os.path.splitext(archivo)[1].lower()
+    formatos = {
+        '.pdf': 'PDF',
+        '.jpeg': 'JPEG',
+        '.jpg': 'JPG',
+        '.jpe': 'JPEG',
+        '.jpg2': 'JPEG2000',
+        '.mp3': 'MP3',
+        '.wav': 'WAVE',
+        '.mpg': 'MPEG-1',
+        '.mp1': 'MPEG-1',
+        '.mp2': 'MPEG-2',
+        '.mp3': 'MPEG-1',
+        '.m1v': 'MPEG-1',
+        '.m1a': 'MPEG-1',
+        '.m2a': 'MPEG-2',
+        '.mpa': 'MPEG',
+        '.mpv': 'MPEG-4',
+        '.mpeg': 'MPEG-1',
+        '.m4v': 'MPEG-4'
+    }
+    return formatos.get(extension, 'Desconocido')
+
+contador = 1
+
+num_archivos = len([f for f in os.listdir(directorio) if os.path.isfile(os.path.join(directorio, f))])
+
+excel_app = win32.gencache.EnsureDispatch('Excel.Application')
+workbook = excel_app.Workbooks.Open(ruta_excel_existente)
+excel_app.Visible = True
+
+for _ in range(num_archivos):
+    excel_app.Run('Macro1InsertarFila')
+
+workbook.Save()
+workbook.Close()
+
+archivos_info = []
 for archivo in os.listdir(directorio):
     ruta_archivo = os.path.join(directorio, archivo)
     if os.path.isfile(ruta_archivo):
         nombre_archivo_original = archivo
         nombre_archivo_limpio = limpiar_nombre_archivo(nombre_archivo_original)
         fecha_creacion = datetime.fromtimestamp(os.path.getctime(ruta_archivo)).strftime('%d/%m/%Y')
-        size_file = os.path.getsize(ruta_archivo) / 1024
-        size_file = f"{size_file:.0f} KB"
+        size_file = round(os.path.getsize(ruta_archivo) / 1024)
+        size_file = f"{size_file} KB"
         
-        num_paginas = 'N/A'
         if archivo.lower().endswith('.pdf'):
             try:
                 reader = PdfReader(ruta_archivo)
                 num_paginas = len(reader.pages)
             except:
                 num_paginas = 'Error al leer'
+        else:
+            num_paginas = 1
 
         archivos_info.append({
+            'Número secuencial': contador,
             'Nombre del archivo': nombre_archivo_limpio,
             'Fecha de creación': fecha_creacion,
             'Número de páginas': num_paginas,
-            'Tamaño del archivo': size_file
+            'Tamaño del archivo': size_file,
+            'Tipo de formato': obtener_tipo_formato(archivo)
         })
+        contador += 1
 
-df = pd.DataFrame(archivos_info)
 
-ruta_excel = os.path.join(directorio, '00IndiceElectronico.xlsx')
-df.to_excel(ruta_excel, index=False)
+wb = load_workbook(ruta_excel_existente, keep_vba=True)
+ws = wb.active
 
-print(f'Información de archivos guardada en {ruta_excel}')
+def write_to_cell(sheet, cell, value):
+    if isinstance(sheet[cell], MergedCell):
+        for range in sheet.merged_cells.ranges:
+            if cell in range:
+                sheet.unmerge_cells(str(range))
+                break
+    sheet[cell] = value
+
+fila_inicial = 11
+for i, archivo_info in enumerate(archivos_info):
+    fila_actual = fila_inicial + i
+    write_to_cell(ws, f'A{fila_actual}', archivo_info['Nombre del archivo'])
+    write_to_cell(ws, f'B{fila_actual}', archivo_info['Fecha de creación'])
+    write_to_cell(ws, f'D{fila_actual}', archivo_info['Número secuencial'])
+    write_to_cell(ws, f'E{fila_actual}', archivo_info['Número de páginas'])
+    write_to_cell(ws, f'I{fila_actual}', archivo_info['Tamaño del archivo'])
+    write_to_cell(ws, f'H{fila_actual}', archivo_info['Tipo de formato'])
+
+wb.save(ruta_excel_nueva)
+
+print(f'Información de archivos guardada en {ruta_excel_nueva}')
